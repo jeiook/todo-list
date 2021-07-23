6f316641cfe1b6687eed65aa77b194d1e8714c41
Jewook Ryu <ryu_jewook@berkeley.edu>
2021-07-23 13:27:07 -0700

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To-do List',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    // rules: [
+    //   {
+    //     test: /\.css$/i,
+    //     use: ['style-loader', 'css-loader'],
+    //   },
+    // ],
+  },
+};
